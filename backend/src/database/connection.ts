3dddae13bc335ccf4a6da2e5c56a6d103834a9bd
brendan-flores <flores.brendanemmanuel@gmail.com>
2026-09-16 21:36:27 +0800

    [Flores][feat] configure Render PostgreSQL database connection with SSL and optimized connection pooling

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -1,16 +1,26 @@
 import { Pool, PoolConfig } from 'pg';
 import { config } from '../config';
 
-const poolConfig: PoolConfig = {
-  host: config.database.host,
-  port: config.database.port,
-  database: config.database.name,
-  user: config.database.user,
-  password: config.database.password,
-  max: 20, // Maximum number of clients in the pool
-  idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
-  connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
-};
+// Use DATABASE_URL if available (preferred for Render/cloud databases), otherwise use individual parameters
+const poolConfig: PoolConfig = config.database.url 
+  ? {
+      connectionString: config.database.url,
+      max: 10, // Reduced pool size for remote connections
+      idleTimeoutMillis: 10000, // Close idle clients after 10 seconds
+      connectionTimeoutMillis: 10000, // Increased timeout for remote connections (10 seconds)
+      statement_timeout: 30000, // Query timeout (30 seconds)
+      query_timeout: 30000, // Query timeout (30 seconds)
+    }
+  : {
+      host: config.database.host,
+      port: config.database.port,
+      database: config.database.name,
+      user: config.database.user,
+      password: config.database.password,
+      max: 20, // Maximum number of clients in the pool
+      idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
+      connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
+    };
 
 export const pool = new Pool(poolConfig);
 
