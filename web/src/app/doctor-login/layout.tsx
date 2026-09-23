76a28bd9a8926683e8f0c1ece7d66e778c108302
brendan-flores <flores.brendanemmanuel@gmail.com>
2026-09-23 21:01:38 +0800

    [Flores][feat] add dedicated doctor and secretary login routes

---
+++ web/src/app/doctor-login/layout.tsx
@@ -0,0 +1,7 @@
+export default function DoctorLoginLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
---
+++ web/src/app/secretary-login/layout.tsx
@@ -0,0 +1,7 @@
+export default function SecretaryLoginLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
